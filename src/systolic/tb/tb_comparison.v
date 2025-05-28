f551f10f2679e1842f0a4003bf63691b8357e66c
DESKTOP\KimYehoon <kimyehoon7262@gmail.com>
2025-05-28 22:28:01 +0900

    fix some typos in tb

--- src/systolic/tb/tb_comparison.v
+++ src/systolic/tb/tb_comparison.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module tb_comparision;
+module tb_comparison;
 
 	reg clk, rst;
 
@@ -27,6 +27,7 @@ module tb_comparision;
 	o00_3, o01_3,
 	o10_3, o11_3;
 
+    // Single PE
 	one_by_one_systolic systolic_1b1 (
 		clk, rst,
 
@@ -43,6 +44,7 @@ module tb_comparision;
 		o10_1, o11_1
 		);
 
+    // 2 By 2 Systolic
 	two_by_two_systolic systolic_2b2 (
 		clk, rst,
 
@@ -58,7 +60,8 @@ module tb_comparision;
 		o00_2, o01_2,
 		o10_2, o11_2
 		);
-		
+
+    // 3 By 3 Systolic
 	three_by_three_systolic_v2 systolic_3b3_v2 (
 		clk, rst,
 
@@ -84,7 +87,7 @@ module tb_comparision;
 		{i00, i01, i02, i03, i10, i11, i12, i13, i20, i21, i22, i23, i30, i31, i32, i33}
 		<= {8'd252, 8'd165, 8'd199, 8'd27, 8'd93, 8'd28, 8'd86, 8'd176, 8'd149, 8'd110, 8'd113, 8'd249, 8'd234, 8'd207, 8'd29, 8'd30};
 		
-		// Filter Valueu
+		// Filter Value
 		{f00, f01, f02, f10, f11, f12, f20, f21, f22}
 		<= {8'd181, 8'd176, 8'd207, 8'd111, 8'd248, 8'd115, 8'd64, 8'd95, 8'd253};
 		
