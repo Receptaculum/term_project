f7a1aafacb2a474bc996c994d89caaa9da3a3e39
DESKTOP\KimYehoon <kimyehoon7262@gmail.com>
2025-05-28 17:34:29 +0900

    update testbench for controller

--- src/controller/tb_controller.v
+++ src/controller/tb_controller.v
@@ -4,13 +4,13 @@ module tb_controller;
 
     reg clk, rst;
     wire rst_mem, rst_pe, rst_3b3, rst_2b2, rst_disp;
-    wire [2:0] state;
+    
 
     controller cont (
         .clk(clk), .rst(rst),
         .rst_mem(rst_mem), .rst_pe(rst_pe),
         .rst_3b3(rst_3b3), .rst_2b2(rst_2b2),
-        .rst_disp(rst_disp), .state(state)
+        .rst_disp(rst_disp)
     );
 
     // Clock generator: 100 MHz (10ns period)
@@ -21,13 +21,13 @@ module tb_controller;
     initial begin
         rst = 1;
         #5 rst = 0;
-        #500 rst = 1;
+        #200 rst = 1;
     end
 
     // Monitor state changes
     initial begin
-        $display("Time(ns)\tState");
-        $monitor("%0t\t\t%d", $time, state);
+        $display("Time(ns)\t");
+        $monitor("%0t\t", $time);
     end
 
 endmodule
