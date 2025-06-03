2ab9a5a133f0a2cfd9172d85c2a0cb6079b60a1d
DESKTOP\KimYehoon <kimyehoon7262@gmail.com>
2025-06-03 20:46:14 +0900

    update testbench for calculation

--- src/systolic/tb/tb_three_by_three_systolic.v
+++ src/systolic/tb/tb_three_by_three_systolic.v
@@ -32,7 +32,7 @@ module tb_three_by_three_systolic;
 		f20, f21, f22,
 
 		o00, o01,
-		o10, o11
+		o10, o11,
 		);
 
 	initial forever begin
@@ -41,21 +41,20 @@ module tb_three_by_three_systolic;
 	
 	initial begin
 		{i00, i01, i02, i03, i10, i11, i12, i13, i20, i21, i22, i23, i30, i31, i32, i33}
-		<= {8'd9, 8'd8, 8'd2, 8'd6, 8'd0, 8'd4, 8'd1, 8'd6, 8'd4, 8'd10, 8'd1, 8'd1, 8'd2, 8'd2, 8'd9, 8'd9};
+		<= {8'd8, 8'd3, 8'd9, 8'd1, 8'd7, 8'd7, 8'd2, 8'd8, 8'd5, 8'd6, 8'd3, 8'd1, 8'd4, 8'd9, 8'd2, 8'd6};
 		
 		{f00, f01, f02, f10, f11, f12, f20, f21, f22}
-		<= {8'd3, 8'd2, 8'd0, 8'd2, 8'd0, 8'd1, 8'd3, 8'd1, 8'd1};
+		<= {8'd1, 8'd5, 8'd8, 8'd6, 8'd0, 8'd7, 8'd3, 8'd1, 8'd2};
 		
 		clk <= 0;
 		rst <= 1;
-		#200
+		#30
 		
 		rst <= 0;
 		#500
-		
 		rst <= 1;
-		#200
-		rst <= 0;
+		#100
+        $finish;
 	end
 
 endmodule
